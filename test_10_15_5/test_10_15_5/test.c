762da8259f27ff267675e56dfba217cd3f25ad81
HaiboYuCoder <3218541421@qq.com>
2022-10-15 20:40:48 +0800

    请计算表达式“(-8+22)×a-10+c÷2”，其中，a = 40，c = 212。

---
+++ test_10_15_5/test_10_15_5/test.c
@@ -0,0 +1,14 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+    int a = 40;
+    int c = 212;
+    int answer = 0;
+    answer = (-8 + 22) * a - 10 + c / 2;
+    printf("%d\n", answer);
+
+
+
+    return 0;
+}
