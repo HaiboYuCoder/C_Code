c7882a7a6fdcc5eb07b0ee583fdcb9dea29a7ba9
HaiboYuCoder <3218541421@qq.com>
2022-10-15 19:58:43 +0800

    1

---
+++ test_10_15_1/test_10_15_1/test.c
@@ -0,0 +1,15 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+int main()
+{
+	//%c - ´òÓ¡×Ö·û
+	char ch = 'w';
+	printf("%c\n", ch);
+
+	//¸¡µãÊý
+	float weight = 70.1f;
+	double pai = 3.14;
+	
+	return 0;
+}
