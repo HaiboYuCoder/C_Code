7e2b6e6ddc1abe907eb9d3952e9e0c9235023fe8
HaiboYuCoder <3218541421@qq.com>
2022-10-17 12:18:32 +0800

    计算Y的值

---
+++ test_10_17_2/test_10_17_2/test.c
@@ -0,0 +1,16 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+int main()
+{
+    int x = 0;
+    int y = 0;
+    scanf("%d", &x);
+    if (x < 0)
+        printf("1", y);
+    else if(x == 0)
+        printf("0", y);
+    else
+    printf("-1", y);
+    return 0;
+}
