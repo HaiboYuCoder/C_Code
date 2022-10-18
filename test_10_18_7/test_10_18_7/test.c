7e4f983bcf98c3ce5c6100ab3e35c8b6cf9aa884
HaiboYuCoder <3218541421@qq.com>
2022-10-18 17:21:41 +0800

    注释

---
+++ test_10_18_7/test_10_18_7/test.c
@@ -0,0 +1,20 @@
+﻿#define  _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int Add(int a, int b)
+{
+	return a + b;
+}
+/*C语言风格注释
+int Sub(int a, int b)
+{
+    return a-b;
+}
+*/
+int main()
+{
+	//C++注释风格
+	//int a = 10;
+	//调用Add函数，完成加法
+	printf("%d\n", Add(1, 2));
+	return 0;
+}
