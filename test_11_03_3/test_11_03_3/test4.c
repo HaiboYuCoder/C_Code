e9ba448759b2c7eb2ab0034d440f6b8f07cc7ce7
HaiboYuCoder <3218541421@qq.com>
2022-11-03 23:01:36 +0800

    1

---
+++ test_11_03_3/test_11_03_3/test4.c
@@ -0,0 +1,22 @@
+//#define  _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//#include <string.h>
+//
+//int my_strlen(char* pa)
+//{
+//	if (*pa != '\0')
+//		return 1 + my_strlen(pa + 1);
+//	else
+//		return 0;
+//}
+//
+//int main()
+//{
+//	char arr[] = "heu";
+//	//[h e u \0]
+//	//数组名其实就是数组首个元素的地址
+//	//
+//	int ret = my_strlen(arr);
+//	printf("%d\n", ret);
+//	return 0;
+//}
