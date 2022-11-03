71c93823dd5d5b3d18a76978d2eb9fb8a7b807d1
HaiboYuCoder <3218541421@qq.com>
2022-11-03 20:31:39 +0800

    1

---
+++ test_11_03_2/test_11_03_2/test.c
@@ -0,0 +1,21 @@
+//#define  _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//
+////函数的定义
+//int Add(int x, int y)
+//{
+//	return x + y;
+//}
+//
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d %d", &a, &b);
+//	int ret = Add(a, b);
+//	printf("%d\n", ret);
+//
+//	return 0;
+//}
+
+
---
+++ test_11_03_2/test_11_03_2/test2.c
@@ -0,0 +1,14 @@
+//#define  _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//#include "add.h"
+//
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d %d", &a, &b);
+//	int ret = Add(a, b);
+//	printf("%d\n", ret);
+//	return 0;
+//}
+
